66426c1b29eb9e71d79736065a6ce5dd09fe79dc
adityan619 <askaditya619@gmail.com>
2015-12-02 16:49:42 +0530

    commit with sharing Intents of details

--- app/src/main/java/com/adityan/apps/sunshinenew/DetailActivity.java
+++ app/src/main/java/com/adityan/apps/sunshinenew/DetailActivity.java
@@ -19,12 +19,16 @@ package com.adityan.apps.sunshinenew;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
@@ -35,7 +39,7 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -67,9 +71,14 @@ public class DetailActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        public static final String SHARE_HASHTAG = " #SunshineNewApp";
+        private String mForeCastStr;
 
-        public PlaceholderFragment() {
+        public DetailFragment() {
+
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -80,12 +89,42 @@ public class DetailActivity extends ActionBarActivity {
             Intent currentIntent = getActivity().getIntent();
             // The currentIntent Envolope is sent with Extra Text(forecast)
             if (currentIntent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = currentIntent.getStringExtra(Intent.EXTRA_TEXT);
+                mForeCastStr = currentIntent.getStringExtra(Intent.EXTRA_TEXT);
                 // We get the TextView in the fragment_detail by using detail_text id
                 ((TextView)rootView.findViewById(R.id.detail_text))
-                        .setText(forecastStr);
+                        .setText(mForeCastStr);
             }
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu,MenuInflater inflater) {
+            inflater.inflate(R.menu.share_detailfragment,menu);
+            // Get Share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            ShareActionProvider mShareActionProvider = (ShareActionProvider)menuItem.getActionProvider();
+
+//            Now Attach an intent to this mShareActionProvider
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            // If you have something to be auto shared, or you need to share from diff method,
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }
+            else {
+                Log.d(LOG_TAG,"Share Action provider got null?");
+            }
+        }
+
+        public Intent createShareForecastIntent(){
+            Intent sendIntet = new Intent();
+            sendIntet.setAction(Intent.ACTION_SEND);
+            // To come back to Original task after sharing.
+            sendIntet.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            sendIntet.setType("text/plain");
+            sendIntet.putExtra(Intent.EXTRA_TEXT,mForeCastStr+SHARE_HASHTAG);
+            return sendIntet;
         }
     }
 }
